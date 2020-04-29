3a3b76e73d4dd71f8adfb964b0002e3ffc980bcd
lean prakort <prakort@leans-iMac.local>
2020-04-29 18:05:07 -0400

    add remove stock

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,4 +9,14 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was succesfully added"
     redirect_to portfolio_path
   end
+
+  def destroy
+    
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.name} has been removed."
+    redirect_to portfolio_path
+
+  end
 end
