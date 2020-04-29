d8d8d33ed8e8f43304b7511bae398497e39c2b59
lean prakort <prakort@leans-iMac.local>
2020-04-29 14:48:06 -0400

    fix nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  #name of function match ==> the view html.erb
+  #the view has access to profile
   def profile
+    @tracked_stocks = current_user.stocks
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,10 @@
 class Stock < ApplicationRecord
 
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+  
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: 'Tpk_4a5046887edf4a19832822a2bdd5765d',
